211494179f1bcf930814372167f67212c8114b05
Jeffrey Rose <jtr219@outlook.com>
2021-12-09 12:19:59 -0500

    clean up SQL column names

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -1,39 +1,39 @@
 CREATE DATABASE clean_blog; 
 
 CREATE TABLE users (
-  userID SERIAL PRIMARY KEY,
+  user_id SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   user_password VARCHAR(255) NOT NULL,
-  date_joined TIMESTAMPTZ NOT NULL
+  created_at TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE posts (
-  postID SERIAL PRIMARY KEY,
-  userID INT REFERENCES Users(userID) NOT NULL,
+  post_id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES Users(user_id) NOT NULL,
   title VARCHAR(255) NOT NULL,
-  subTitle VARCHAR(255), -- nullable
+  subtitle VARCHAR(255), -- nullable
   post_text TEXT NOT NULL, -- may change to JSONB based on react editor
-  date_posted TIMESTAMPTZ NOT NULL
+  created_at TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE comments (
-  commentID SERIAL PRIMARY KEY,
-  responseToCommentID INT REFERENCES Comments (commentID), -- nullable, used when one comment is in response to another comment, nesting it
-  postID INT REFERENCES Posts(postID) NOT NULL,
-  userID INT REFERENCES Users(userID) NOT NULL,
+  comment_id SERIAL PRIMARY KEY,
+  response_to_comment_id INT REFERENCES Comments (comment_id), -- nullable, used when one comment is in response to another comment, nesting it
+  post_id INT REFERENCES Posts(post_id) NOT NULL,
+  user_id INT REFERENCES Users(user_id) NOT NULL,
   comment_text TEXT NOT NULL,
-  date_posted TIMESTAMPTZ NOT NULL
+  created_at TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE post_votes (
-  postID INT REFERENCES Posts(postID) NOT NULL,
-  voteValue INT NOT NULL, -- 1, 0, or -1
-  userID INT REFERENCES Users(userID) NOT NULL
+  post_id INT REFERENCES Posts(post_id) NOT NULL,
+  vote_value INT NOT NULL, -- 1, 0, or -1
+  user_id INT REFERENCES Users(user_id) NOT NULL
 );
 
 CREATE TABLE comment_votes (
-  commentID INT REFERENCES Comments(commentID) NOT NULL,
-  voteValue INT NOT NULL, -- 1, 0, or -1
-  userID INT REFERENCES Users(userID) NOT NULL
+  comment_id INT REFERENCES Comments(comment_id) NOT NULL,
+  vote_value INT NOT NULL, -- 1, 0, or -1
+  user_id INT REFERENCES Users(user_id) NOT NULL
 );
