889b7d0abcd4d4a76a1826bcba7b48a72012fedc
Jeffrey Rose <jtr219@outlook.com>
2021-12-10 14:21:43 -0500

    add ConvertUser function

--- modelConverters/modelConverters.go
+++ modelConverters/modelConverters.go
@@ -15,10 +15,26 @@ func ConvertPost(sql_post *sql_models.Post) gql_models.Post {
 		CreatedAt: sql_post.CreatedAt,
 	}
 }
+/*
+func ConvertComment(sql_comment *sql_models.Comment) gql_models.Comment {
+	return gql_models.Comment{
+		CommentID: sql_comment.CommentID,
+		ResponseToCommentID: sql_comment.ResponseToCommentID, // change to 0's, nonnullable
+		PostID: sql_comment.PostID,
+		UserID: sql_comment.UserID,
+		CommentText: sql_comment.CommentText,
+		CreatedAt: sql_comment.CreatedAt,
+	}
+}*/
 
-func ConvertComment() {}
-
-func ConvertUser() {}
+func ConvertUser(sql_user *sql_models.User) gql_models.User {
+	return gql_models.User{
+		UserID: sql_user.UserID,
+		Username: sql_user.Username,
+		Email: sql_user.Email,
+		CreatedAt: sql_user.CreatedAt,
+	}
+}
 
 func ConvertCommentVote() {}
 
