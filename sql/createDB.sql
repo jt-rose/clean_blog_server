7049967d4641d276e9f96d7f64abd964e0edad3b
Jeffrey Rose <jtr219@outlook.com>
2021-12-09 21:14:50 -0500

    add composite primary keys for sqlboiler codegen

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -29,11 +29,13 @@ CREATE TABLE comments (
 CREATE TABLE post_votes (
   post_id INT REFERENCES Posts(post_id) NOT NULL,
   vote_value INT NOT NULL, -- 1, 0, or -1
-  user_id INT REFERENCES Users(user_id) NOT NULL
+  user_id INT REFERENCES Users(user_id) NOT NULL,
+  PRIMARY KEY(post_id, user_id)
 );
 
 CREATE TABLE comment_votes (
   comment_id INT REFERENCES Comments(comment_id) NOT NULL,
   vote_value INT NOT NULL, -- 1, 0, or -1
-  user_id INT REFERENCES Users(user_id) NOT NULL
+  user_id INT REFERENCES Users(user_id) NOT NULL,
+  PRIMARY KEY(comment_id, user_id)
 );
