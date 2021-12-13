2241b497e974fa61f1c6f386a8579d1e543452d0
Jeffrey Rose <jtr219@outlook.com>
2021-12-12 23:42:04 -0500

    add constants for hash and block keys

--- constants/constants.go
+++ constants/constants.go
@@ -8,24 +8,28 @@ import (
 )
 
 type ENV_Variables struct {
-	DATABASE_URL string
+	DATABASE_URL  string
 	DATABASE_PORT string
-	SERVER_PORT string
+	SERVER_PORT   string
+	HASH_KEY      string
+	BLOCK_KEY     string
 }
 
 func loadEnvVariables() ENV_Variables {
 	err := godotenv.Load()
 	if err != nil {
-	  log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file")
 	}
 
 	ENV_VAR := ENV_Variables{
-		DATABASE_URL: os.Getenv("DATABASE_URL"),
-	DATABASE_PORT: os.Getenv("DATABASE_URL"),
-	SERVER_PORT: os.Getenv("SERVER_PORT"),
+		DATABASE_URL:  os.Getenv("DATABASE_URL"),
+		DATABASE_PORT: os.Getenv("DATABASE_URL"),
+		SERVER_PORT:   os.Getenv("SERVER_PORT"),
+		HASH_KEY:      os.Getenv("HASH_KEY"),
+		BLOCK_KEY:     os.Getenv("BLOCK_KEY"),
 	}
 
-	if ENV_VAR.DATABASE_URL == "" || ENV_VAR.DATABASE_PORT == "" || ENV_VAR.SERVER_PORT == "" {
+	if ENV_VAR.DATABASE_URL == "" || ENV_VAR.DATABASE_PORT == "" || ENV_VAR.SERVER_PORT == "" || ENV_VAR.HASH_KEY == "" || ENV_VAR.BLOCK_KEY == "" {
 		log.Fatal("Error loading .env file")
 	}
 
--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
