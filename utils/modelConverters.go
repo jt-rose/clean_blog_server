9dee011213364586a32a078f889742524ca15f8c
Jeffrey Rose <jtr219@outlook.com>
2021-12-19 02:20:52 -0500

    add votevalue enum converter

--- utils/modelConverters.go
+++ utils/modelConverters.go
@@ -5,6 +5,32 @@ import (
 	sql_models "github.com/jt-rose/clean_blog_server/sql_models"
 )
 
+func ConvertGQLVoteValueEnums(voteValue gql_models.VoteValue) int {
+	switch voteValue {
+	case gql_models.VoteValueUpvote:
+		return 1;
+	case gql_models.VoteValueDownvote:
+		return -1;
+	case gql_models.VoteValueNeutral:
+		return 0;
+	default:
+		return 0;
+	}
+}
+
+func ConvertSQLVoteValueEnums(voteValue int) gql_models.VoteValue {
+	switch voteValue {
+	case 1:
+		return gql_models.VoteValueUpvote;
+	case -1:
+		return gql_models.VoteValueDownvote;
+	case 0:
+		return gql_models.VoteValueNeutral;
+	default:
+		return gql_models.VoteValueNeutral;
+	}
+}
+
 func ConvertPost(sql_post *sql_models.Post) gql_models.Post {
 	return gql_models.Post{
 		UserID: sql_post.UserID,
@@ -50,7 +76,7 @@ func ConvertUser(sql_user *sql_models.User) gql_models.User {
 func ConvertCommentVote(sql_comment_vote *sql_models.CommentVote) gql_models.CommentVote {
 	return gql_models.CommentVote{
 		CommentID: sql_comment_vote.CommentID,
-		VoteValue: sql_comment_vote.VoteValue,
+		VoteValue: ConvertSQLVoteValueEnums(sql_comment_vote.VoteValue),
 		UserID: sql_comment_vote.UserID,
 	}
 }
@@ -58,7 +84,7 @@ func ConvertCommentVote(sql_comment_vote *sql_models.CommentVote) gql_models.Com
 func ConvertPostVote(sql_post_vote *sql_models.PostVote) gql_models.PostVote {
 	return gql_models.PostVote{
 		PostID: sql_post_vote.PostID,
-		VoteValue: sql_post_vote.VoteValue,
+		VoteValue: ConvertSQLVoteValueEnums(sql_post_vote.VoteValue),
 		UserID: sql_post_vote.UserID,
 	}
 }
