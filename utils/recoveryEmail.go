b4e6adb5508f8a92a347738874bd341941d5823d
Jeffrey Rose <jtr219@outlook.com>
2021-12-27 02:11:41 -0500

    set up password recovery email

--- utils/recoveryEmail.go
+++ utils/recoveryEmail.go
@@ -7,21 +7,22 @@ import (
 	"github.com/jt-rose/clean_blog_server/constants"
 )
 
-
-func SendSampleEmail() {
+func SendPasswordResetEmail(recieverEmail string, resetKey string) error {
 	// sender data
 	from := constants.ENV_VARIABLES.EMAIL_ADDRESS
 	password := constants.ENV_VARIABLES.EMAIL_PASSWORD
 	// receiver address
-	toEmail := constants.ENV_VARIABLES.SAMPLE_TO_EMAIL
+	toEmail := recieverEmail
 	to := []string{toEmail}
 	// smtp - Simple Mail Transfer Protocol
 	host := "smtp.gmail.com"
 	port := "587"
 	address := host + ":" + port
 	// message
-	subject := "Subject: Our Golang Email\n"
-	body := "our first email!"
+	subject := "Clean Blog Password Reset Link\n"
+	body := "A request to reset your password on Clean Blog was recently made. Please visit the following link to reset your password:\n" +
+	fmt.Sprintf("%s/reset-password/%s", constants.ENV_VARIABLES.FRONTEND_URL, resetKey)
+
 	message := []byte(subject + body)
 	// athentication data
 	// func PlainAuth(identity, username, password, host string) Auth
@@ -30,8 +31,8 @@ func SendSampleEmail() {
 	// func SendMail(addr string, a Auth, from string, to []string, msg []byte) error
 	err := smtp.SendMail(address, auth, from, to, message)
 	if err != nil {
-		fmt.Println("err:", err)
-		return
+		return err
 	}
-	fmt.Println("mail sent")
+	fmt.Println("password reset requested")
+	return nil
 }
